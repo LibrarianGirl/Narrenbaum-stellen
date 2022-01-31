65fbb4dbeaf2f378301da57f1218bb7df1dd8c86
LibrarianGirl <AnnaWe95@t-online.de>
2022-01-31 20:20:56 +0100

    Create programm.js

---
+++ programm.js
@@ -0,0 +1,28 @@
+<body>
+ // <script> //um den Button zu aktivieren und deaktivieren
+	//function enable(){    
+		//document.getElementById("btn").disabled = true;}
+
+	//function disable(){    
+	//	document.getElementById("btn").disabled = false;}
+//	</script>
+
+  <img id="img" src="https://via.placeholder.com/350x150.png" />
+  <button onClick="rotateImg()" id="btn" >Rotate Image</button>
+
+  <script>
+    let rotation = 0;
+    function rotateImg() {
+    
+      rotation += 10; // add 90 degrees, you can change this as you want
+      if (rotation === 90) { 
+        // 90 means rotate back to 0
+        rotation = 0;
+      }
+      document.querySelector("#img").style.transform = `rotate(${rotation}deg)`;
+     document.getElementById("btn").disabled = true;
+    	setTimeout(function(){ document.getElementById("btn").disabled = false
+		}, 5000);
+    }
+  </script>
+</body>
